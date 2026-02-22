dac1cf01e72a6d738dfd783f69775af70896dff6
pixia1234 <pixia2005@gmail.com>
2026-02-22 12:51:51 +0000

    chore(release): bump version to 0.3.5

--- go-gost/version.go
+++ go-gost/version.go
@@ -1,13 +1,13 @@
 package main
 
 var (
-	version = "0.3.4"
+	version = "0.3.5"
 )
 
 func normalizedVersion() string {
 	v := version
 	if v == "" {
-		return "0.3.4"
+		return "0.3.5"
 	}
 	return v
 }
